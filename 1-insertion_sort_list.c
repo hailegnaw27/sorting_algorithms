045896b44175c4f4bcb8cb64967bba621998e7f5
hailegnaw27 <ixvos406@gmail.com>
2023-11-18 12:34:27 +0000

    sorting algorithim

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,37 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Applies the insertion sort algorithm on a doubly linked list
- * @list: Pointer to the head of the doubly linked list
- */
+* insertion_sort_list - Applies the insertion sort algorithm on a doubly linked list
+* @list: Pointer to the head of the doubly linked list
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr = *list, *tmp = NULL;
+listint_t *curr = *list, *tmp = NULL;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+if (!list || !(*list) || !(*list)->next)
+return;
 
-    curr = curr->next;
-    while (curr != NULL)
-    {
-        tmp = curr;
-        curr = curr->next;
+curr = curr->next;
+while (curr != NULL)
+{
+tmp = curr;
+curr = curr->next;
 
-        while (tmp->prev && tmp->prev->n > tmp->n)
-        {
-            tmp->prev->next = tmp->next;
-            if (tmp->next)
-                tmp->next->prev = tmp->prev;
-            tmp->next = tmp->prev;
-            tmp->prev = tmp->next->prev;
-            tmp->next->prev = tmp;
+while (tmp->prev && tmp->prev->n > tmp->n)
+{
+tmp->prev->next = tmp->next;
+if (tmp->next)
+tmp->next->prev = tmp->prev;
+tmp->next = tmp->prev;
+tmp->prev = tmp->next->prev;
+tmp->next->prev = tmp;
 
-            if (tmp->prev == NULL)
-                *list = tmp;
-            else
-                tmp->prev->next = tmp;
+if (tmp->prev == NULL)
+*list = tmp;
+else
+tmp->prev->next = tmp;
 
-            print_list(*list);
-        }
-    }
+print_list(*list);
+}
+}
 }
